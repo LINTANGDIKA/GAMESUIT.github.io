17037a6d4614589613c22cf1c21bfd4bdf5d4ede
LINTANG ANDIKA SAPUTRA <lintangdik0@gmail.com>
2021-02-10 20:40:52 +0700

    Html to script [Lintang]

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // Pengambilan dari HTML
+let hasil = document.querySelector('.hasil h1');
+let style = document.querySelector('.hasil');
+let player = document.querySelectorAll('.player img');
+let computer = document.querySelectorAll('.computer img');
+let refresh = document.querySelectorAll('.refresh img');
+// Akhir Pengambilan Html
