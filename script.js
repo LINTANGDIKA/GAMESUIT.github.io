5acae912eee0e5989665bc5053e4755fe7f8aff5
LINTANG ANDIKA SAPUTRA <lintangdik0@gmail.com>
2021-02-11 21:40:39 +0700

    edit style script and info [Lintang]

--- script.js
+++ script.js
@@ -7,14 +7,17 @@ let computer = document.querySelectorAll('.computer img');
 let refresh = document.querySelector('.refresh img');
 // Akhir Pengambilan Html
 class sistemmain {
+    // sistem game menang & kalah
     sistem (player, computer) {
         if (player == computer) return 'SERI!';
         if ( player == 'batu') return (computer == 'gunting') ? 'PLAYER 1 WIN' : 'COMP WIN';
         if ( player == 'kertas') return (computer == 'batu') ? 'PLAYER 1 WIN' : 'COMP WIN';
         if ( player == 'gunting') return (computer == 'guntkeing') ? 'PLAYER 1 WIN' : 'COMP WIN';
     }  
+    // akhir
 }
 class random extends sistemmain {
+    // random tempat serta random hasil
     computer () {
         let pilihan = ["batu", "gunting", "kertas"];
         let angkaComputer = Math.floor(Math.random()*pilihan.length);
@@ -29,14 +32,37 @@ class random extends sistemmain {
     
         return random;
     }
+    // akhir
 }
-
+class style extends sistemmain {
+    // style saat menang dan kalah
+    windraw () {
+        if (hasil.innerHTML == "SERI!") {
+            tampil.classList.add('draw');
+            setTimeout(()=>{
+                tampil.classList.remove('draw');
+            },2000)
+        }else if (hasil.innerHTML == "PLAYER 1 WIN") {
+            tampil.classList.add('win');
+            setTimeout(()=>{
+                tampil.classList.remove('win');
+            },2000);
+        }else {
+            tampil.classList.add('win');
+            setTimeout(()=>{
+                tampil.classList.remove('win');
+            },2000);
+        }
+    }
+    // akhir
+}
+// Pengambilan kelas 
 let main = new sistemmain();
 let bot = new random();
+let warna = new style();
+// akhir pengambilan kelas
 
-
-
-
+// ketika memainkan game
 player.forEach(pemain => {
     pemain.addEventListener('click',()=>{
        let player = pemain.className;
@@ -52,7 +78,7 @@ player.forEach(pemain => {
        hasil.textContent = maingame;
        tampil.classList.remove('none');
        vs.classList.add('none')
-       
+       let warnahasil = warna.windraw(); 
             setTimeout(()=>{
                 tampil.classList.add('none');
             },2000)
@@ -60,6 +86,9 @@ player.forEach(pemain => {
        
     })
 });
+// akhir 
+
+// untuk mengembalikan seperti semula
 refresh.addEventListener('click',()=>{
     refresh.animate([
         { transform: 'rotate(-180deg)'}
@@ -71,4 +100,5 @@ refresh.addEventListener('click',()=>{
     computer [0].setAttribute('src', 'assets/' + `batu.png`);
     computer [1].setAttribute('src', 'assets/' + `kertas.png`);
     computer [2].setAttribute('src', 'assets/' + `gunting.png`);
-})
+});
+// akhir
